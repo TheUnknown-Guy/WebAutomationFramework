b2155b682d09dcd64121f058e6b84597439b5b85
TheUnknown-Guy <saczjagad@gmail.com>
2021-02-28 13:37:16 +0530

    removing assert from reusable methods

--- WebApplicationTestingFramework/src/test/java/utility/ReusableFunctions.java
+++ WebApplicationTestingFramework/src/test/java/utility/ReusableFunctions.java
@@ -18,7 +18,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
+
 
 import base.BaseClass;
 
